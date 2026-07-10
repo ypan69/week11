49ed4db32a23ddcfb2219afaa769ead36b65fdc3
ypan69 <ypan69@myseneca.ca>
2026-07-10 11:54:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -62,6 +62,7 @@ resource "aws_instance" "web" {
 
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
+  description = "Allow HTTP access from admin"
   ingress {
     from_port   = 8080
     to_port     = 8080
